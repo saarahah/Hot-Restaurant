450a86bf75a544a7b58508285f08d3f8fa068ce1
EpicGibbon <michant1997@gmail.com>
2020-07-09 14:13:07 -0400

    have the intial routes going

--- server.js
+++ server.js
@@ -22,13 +22,25 @@ app.use(express.json());
 
 
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function (req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+// Displays all characters
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"))
+});
+
 
 
 
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-  
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
